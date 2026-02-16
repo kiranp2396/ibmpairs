1cce14d609d350226b11a597105386d2380a908c
Kiran Patel <Kiran.Patel@ibm.com>
2026-02-16 11:11:27 -0600

    Release 3.1.16
    Signed-off-by: Kiran Patel <Kiran.Patel@ibm.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 # define package version
-version = '3.1.15'
+version = '3.1.16'
 
 # ... and record it for ibmpairs package
 with open("ibmpairs/version.py", 'w') as f:
